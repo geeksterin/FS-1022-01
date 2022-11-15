c2d888f597337b3a04397f764761ae307909aba6
Aman Srivastava <aman4863@gmail.com>
2022-11-15 19:51:46 +0530

    added

---
+++ lec001/02. printVsprintln/Main.java
@@ -0,0 +1,12 @@
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.print("Hello World 1");
+		System.out.print("Hello World 2");
+
+
+        System.out.println("Next Line 1");
+		System.out.println("Next Line 2");
+    
+	}
+}
