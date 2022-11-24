71e869e7d4e2bd05964856cd51577920684705a7
Aman Srivastava <aman4863@gmail.com>
2022-11-24 21:24:17 +0530

    added

--- lec008/02. addIfADigit/First.java
+++ lec008/02. addIfADigit/First.java
@@ -4,19 +4,17 @@ import java.util.*;
 public class Solution {
 
     public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner scn = new Scanner(System.in);
         
         char ch = scn.next().charAt(0);
         
-        if(Character.isDigit(ch)){              // if(ch>='0' && ch <='9')
-            int val = ch - '0';
+        if(Character.isDigit(ch)){
+            int val = Character.getNumericValue(ch);
             val = val + 100;
             System.out.println(val);
-            
-        }
-        else{
+        }else{
             System.out.println("This is not a digit");
         }
-        
     }
 }
