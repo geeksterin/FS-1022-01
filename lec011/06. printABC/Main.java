d98a0ea5c0f22d42862afe0211981416dd9c7e65
Aman Srivastava <aman4863@gmail.com>
2022-11-30 21:42:18 +0530

    added

---
+++ lec011/06. printABC/Main.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        for(char ch = 'a'; ch <= 'z'; ch++){
+            System.out.println(ch);
+        }
+    }
+}
