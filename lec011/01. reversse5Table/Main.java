fdc5850e1820ddd7ad6e97651fce0bd3d043dc94
Aman Srivastava <aman4863@gmail.com>
2022-11-30 20:18:20 +0530

    added

---
+++ lec011/01. reversse5Table/Main.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        for(int i = 10; i >= 1; i--){
+            System.out.println("5x" + i + "=" + i*5 );
+        }
+    }
+}
