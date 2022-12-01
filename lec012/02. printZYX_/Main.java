4a8be9dcf1ad758e6469863d46dade394a1e4712
Aman Srivastava <aman4863@gmail.com>
2022-12-01 20:35:58 +0530

    added

---
+++ lec012/02. printZYX_/Main.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        for(char ch = 'z' ; ch >= 'a'; ch--){
+            System.out.println(ch);
+        }
+    }
+}
