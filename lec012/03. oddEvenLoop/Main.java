6076bb050d62d5dc525539396dc415e40b253112
Aman Srivastava <aman4863@gmail.com>
2022-12-01 20:48:39 +0530

    added

---
+++ lec012/03. oddEvenLoop/Main.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        
+        for(int i = 1; i <= n; i++){
+            int val = scn.nextInt();
+            if(val % 2 == 0){
+                System.out.println("even");
+            }else{
+                System.out.println("odd");
+            }
+        }
+    }
+}
