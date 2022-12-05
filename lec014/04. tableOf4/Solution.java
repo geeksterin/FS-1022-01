34a480564724bf7d2493fe16800d2d9b55a43f73
Aman Srivastava <aman4863@gmail.com>
2022-12-05 21:45:33 +0530

    added

---
+++ lec014/04. tableOf4/Solution.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int i = 1;
+        
+        while(i <= 10){
+            System.out.println("4x" + i + "=" + 4*i);
+            i++;
+        }
+        
+        
+    }
+}
