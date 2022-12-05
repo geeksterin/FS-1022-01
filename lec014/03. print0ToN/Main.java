9770cd6c6522afc46c64aa96ec2180a8958f924c
Aman Srivastava <aman4863@gmail.com>
2022-12-05 21:43:25 +0530

    added

---
+++ lec014/03. print0ToN/Main.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        
+        int n = scn.nextInt();
+        
+        
+        
+        int i = 0;
+        while(i <= n){
+            System.out.println(i);
+            i++;
+        }
+        
+        
+    }
+}
