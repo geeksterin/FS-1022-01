59dd496b1fa56e02ea11e12ee03382a9e8b0843a
Aman Srivastava <aman4863@gmail.com>
2022-12-05 21:50:59 +0530

    added

---
+++ lec014/05. multipleOf7/Solution.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        
+        int i = 0;
+        
+        while(i <= n){
+            System.out.print(i + " ");
+            i += 7;
+        }
+        
+        
+        
+    }
+}
