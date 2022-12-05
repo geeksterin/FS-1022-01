68af545a48dbb41908bc4fb81f2686ab298ded93
Aman Srivastava <aman4863@gmail.com>
2022-12-05 21:57:20 +0530

    added

---
+++ lec014/06. printXtoN/Solution.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        
+        int x =scn.nextInt();
+        int n =scn.nextInt();
+
+        while(x <= n){
+            System.out.println(x);
+            x++;
+        }
+       
+      
+    }
+}
