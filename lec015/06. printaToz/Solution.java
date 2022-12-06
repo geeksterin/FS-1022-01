ef8d24fd0a011ae52f6d9c875f07a7b5364823e2
Aman Srivastava <aman4863@gmail.com>
2022-12-06 20:52:50 +0530

    added

---
+++ lec015/06. printaToz/Solution.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        char ch = 'a';
+        while(ch <= 'z'){
+            System.out.println(ch);
+            ch++;
+            
+            // ch = (char)(ch + 1);
+        }
+    }
+}
