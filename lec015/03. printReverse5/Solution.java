3b239f410a2e752f7b55a876c715c3b170bdeded
Aman Srivastava <aman4863@gmail.com>
2022-12-06 20:26:30 +0530

    added

---
+++ lec015/03. printReverse5/Solution.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int i = 10;
+        while(i >= 1){
+            System.out.println("5x" + i + "=" + 5*i);
+            i--;
+        }
+    }
+}
