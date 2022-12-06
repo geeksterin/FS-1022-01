0d4ec4c8cde0a84021e47d91d7402e9fad1f4db2
Aman Srivastava <aman4863@gmail.com>
2022-12-06 20:16:13 +0530

    added

---
+++ lec015/01. print3711/Solution.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int i = 3;
+        while( i < n ){
+            System.out.println(i);
+            i += 4;
+        }
+    }
+}
