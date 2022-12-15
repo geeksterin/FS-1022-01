89c8c1deb89b9445c15084d667d344f11263a63e
Aman Srivastava <aman4863@gmail.com>
2022-12-15 20:19:01 +0530

    added

---
+++ lec021/01. formNumXY/Solution.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int x = scn.nextInt();
+        int y = scn.nextInt();
+        int num = x * 10 + y;
+        
+        System.out.println(num);
+        
+    }
+}
