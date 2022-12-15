7f8de8ae2e61cb0c40376b9334475d2c4aa0231e
Aman Srivastava <aman4863@gmail.com>
2022-12-15 21:30:03 +0530

    added

--- lec021/04. printCountAndReverse/Solution.java
+++ lec021/04. printCountAndReverse/Solution.java
@@ -4,10 +4,21 @@ import java.util.*;
 public class Solution {
 
     public static int countDigits(int n){
-        
+        int count  = 0;
+        while(n > 0){
+            n /= 10;
+            count++;
+        }
+        return count;
     }
     public static void printReverse(int n){
-        
+        //just solved this one
+        while(n > 0){
+            int d = n % 10;
+            System.out.println(d);
+            //decompose
+            n /= 10;
+        }
     }
     
     public static void main(String[] args) {
