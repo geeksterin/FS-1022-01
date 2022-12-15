4c7c93d594b092a0fc2dd7a39e8635c9f042a7c2
Aman Srivastava <aman4863@gmail.com>
2022-12-15 21:21:47 +0530

    added

---
+++ lec021/04. printCountAndReverse/Solution.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static int countDigits(int n){
+        
+    }
+    public static void printReverse(int n){
+        
+    }
+    
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int totalDigits = countDigits(n);
+        System.out.println(totalDigits);
+        printReverse(n);
+    }
+}
